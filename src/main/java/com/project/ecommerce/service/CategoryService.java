d62ad82c96db769e1cdf03138599bb066df13cbf
Tural <turalsadiqov497@gmail.com>
2022-06-11 18:33:13 +0400

    added system.properties file for heroku

--- src/main/java/com/project/ecommerce/service/CategoryService.java
+++ src/main/java/com/project/ecommerce/service/CategoryService.java
@@ -1,8 +1,6 @@
 package com.project.ecommerce.service;
 
-import com.project.ecommerce.domain.Category;
 import com.project.ecommerce.dto.ResponseDTO;
-import com.project.ecommerce.dto.SubCategoryDTO;
 
 
 public interface CategoryService {
